e7144b466ac678c08bdbef50bccd1b5d638e26f5
Tanya Schlusser <tanya@tickel.net>
2015-11-10 08:21:31 -0600

    better specification of path for .env

--- chipyhourofcode.py
+++ chipyhourofcode.py
@@ -22,8 +22,7 @@ from flask import redirect, render_template, request, url_for
 from flask.views import MethodView
 from jinja2 import Environment, Template
 
-
-with open('.env') as infile:
+with os.path.join(os.path.basename(__file__), '.env') as infile:
     for line in infile:
         if not line.startswith('#'):
             k, v = line.strip().split('=',1)
